7d2c6b1a48890ad52a75cc9e1639f2d6362c5932
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2018-12-18 20:49:09 -0200

    autoClone

--- src/app/autoCommit/autoComplete.js
+++ src/app/autoCommit/autoComplete.js
@@ -1,15 +1,13 @@
 const	shell	=	require('shelljs');
 const moment = require('moment');
 const readline = require('readline');
-const dotenv = require('dotenv').config();
-
 const leitor = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: true
 });
 
-leitor.question(`\n---------------------\nDigite o numero de segundos para \n---------------------\n`, function(answer) {
+leitor.question(`\n---------------------\nDigite o numero de segundos para dar autoCommit \n---------------------\n`, function(answer) {
     let resp = answer;
     setInterval(autoCommit, resp);
     leitor.close();
